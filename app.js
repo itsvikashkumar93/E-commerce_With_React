8f7acae6f01fc23aa3275642f2081b0954b777d6
itsvikashkumar93 <itsvikashkumar8270@gmail.co>
2024-10-29 17:15:01 +0530

    deploying

--- app.js
+++ app.js
@@ -17,4 +17,5 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRoutes);
 app.use("/user", userRoutes);
 app.use("/product", productRoutes);
-app.listen(3000);
+
+app.listen(process.env.PORT || 3000);
